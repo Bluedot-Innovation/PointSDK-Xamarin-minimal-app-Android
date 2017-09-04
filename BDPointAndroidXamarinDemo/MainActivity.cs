636694e2dc3db6a51441f6289caad70c008e7267
Ravindra Wuyyuru <ravi@bluedotinnovation.com>
2017-09-04 13:34:20 +1000

    Added Authentication and logout buttons to Layout. Implemented scrolling log area.

--- BDPointAndroidXamarinDemo/MainActivity.cs
+++ BDPointAndroidXamarinDemo/MainActivity.cs
@@ -13,65 +13,125 @@ using System.Collections.Generic;
 
 namespace BDPointAndroidXamarinDemo
 {
-    [Activity(Label = "BDPointXamarinDemo", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "BDPointXamarinDemo", MainLauncher = true, Icon = "@mipmap/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : Activity, IServiceStatusListener, IApplicationNotificationListener
     {
-        int count = 1;
+        TextView textViewStatusLog;
+        Button authenticateButton;
+        Button logoutButton;
+
+        ServiceManager serviceManager;
 
         public void OnBlueDotPointServiceError(BDError p0)
         {
-            Console.WriteLine("Bluedot service returned error");
+            updateLog("Bluedot service returned error" + p0.Reason);
         }
 
         public void OnBlueDotPointServiceStartedSuccess()
         {
-            Console.WriteLine("Bluedot service started successfully");
+            updateLog("Bluedot service started successfully");
         }
 
         public void OnBlueDotPointServiceStop()
         {
-            Console.WriteLine("Bluedot service stopped");
+            updateLog("Bluedot service stopped");
         }
 
         public void OnCheckedOutFromBeacon(BeaconInfo p0, ZoneInfo p1, int p2, IDictionary<string, string> p3)
         {
-            Console.WriteLine("Bluedot service - OnCheckedOutFromFence.");
+            updateLog("Bluedot service - OnCheckedOutFromFence.");
         }
 
         public void OnCheckedOutFromFence(FenceInfo p0, ZoneInfo p1, int p2, IDictionary<string, string> p3)
         {
-            Console.WriteLine("Bluedot service - OnCheckedOutFromBeacon.");
+            updateLog("Bluedot service - OnCheckedOutFromBeacon.");
         }
 
         public void OnCheckIntoBeacon(BeaconInfo p0, ZoneInfo p1, LocationInfo p2, Proximity p3, IDictionary<string, string> p4, bool p5)
         {
-            Console.WriteLine("Bluedot service - OnCheckIntoBeacon.");
+            updateLog("Bluedot service - OnCheckIntoBeacon.");
         }
 
         public void OnCheckIntoFence(FenceInfo p0, ZoneInfo p1, LocationInfo p2, IDictionary<string, string> p3, bool p4)
         {
-           Console.WriteLine("Bluedot service - OnCheckIntoFence.");
+            updateLog("Bluedot service - OnCheckIntoFence.");
         }
 
         public void OnRuleUpdate(IList<ZoneInfo> p0)
         {
-            Console.WriteLine("Bluedot service - rules downloaded.");
+            updateLog("Bluedot service - rules downloaded.");
         }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            ServiceManager serviceManager = ServiceManager.GetInstance(this);
+            serviceManager = ServiceManager.GetInstance(this);
 
-            serviceManager.SetForegroundServiceNotification(Resource.Id.icon, "Title", "Message", null);
+            serviceManager.SetForegroundServiceNotification(Resource.Mipmap.Icon, "Title", "Message", null);
             serviceManager.SubscribeForApplicationNotification(this);
-            if (!serviceManager.IsBlueDotPointServiceRunning)
-			{
-                serviceManager.SendAuthenticationRequest("performance.test.v193", "fbad63e0-e760-11e6-a1d3-b8ca3a6b879d", "hello@bluedotinnovation.com", this);
-			}
             SetContentView(Resource.Layout.Main);
+
+
+            textViewStatusLog = (TextView)FindViewById(Resource.Id.tvStatusLog);
+
+            authenticateButton = FindViewById<Button>(Resource.Id.authenticate);
+            authenticateButton.Click += (sender, e) =>
+            {
+                startAuthentication();
+
+            };
+
+            logoutButton = FindViewById<Button>(Resource.Id.logout);
+            logoutButton.Click += (sender, e) =>
+            {
+                stopService();
+
+            };
         }
+
+        private void startAuthentication()
+        {
+            if (!serviceManager.IsBlueDotPointServiceRunning)
+            {
+                serviceManager.SendAuthenticationRequest("performance.test.v193", "fbad63e0-e760-11e6-a1d3-b8ca3a6b879d", "hello@bluedotinnovation.com", this);
+                updateLog("Authenticating..");
+            }
+            else
+            {
+                updateLog("Already Authenticated");
+            }
+
+        }
+
+        public void stopService()
+        {
+            if (serviceManager != null)
+            {
+                if (serviceManager.IsBlueDotPointServiceRunning)
+                {
+                    //Call the method stopPointService in ServiceManager to stop Bluedot PointService
+                    serviceManager.StopPointService();
+                    updateLog("Logged Out");
+                }
+                else
+                {
+                    updateLog("Already Logged out");
+                }
+
+            }
+
+        }
+
+
+        private void updateLog(String s)
+        {
+            RunOnUiThread(() =>
+            {
+                textViewStatusLog.Append(s + "\n");
+            });
+        }
+
     }
 }
 
